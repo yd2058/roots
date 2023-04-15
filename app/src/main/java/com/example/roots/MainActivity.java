8a1a45a551eda0bc31b7c048e46dc5e1c91fb131
yd2058 <yd2058@bs.amalnet.k12.il>
2023-04-15 19:06:35 +0300

    update

--- app/src/main/java/com/example/roots/MainActivity.java
+++ app/src/main/java/com/example/roots/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.roots;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -21,24 +22,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tea = (EditText)findViewById(R.id.tea);
+        tea.setText("");
         teb = (EditText)findViewById(R.id.teb);
+        teb.setText("");
         tec = (EditText)findViewById(R.id.tec);
+        tec.setText("");
         tv = (TextView)findViewById(R.id.tv);
     }
-
     public void root(View view) {
-        if(a!=null && b!=null && c!=null){
+
+        if(!(tea.getText().toString().equals("")&&teb.getText().toString().equals("")&&tec.getText().toString().equals(""))){
+
             Intent si = new Intent(this,rootsolve.class);
-            si.putExtra("a",a);
-            si.putExtra("b",b);
-            si.putExtra("c",c);
+            si.putExtra("a",Double.valueOf(tea.getText().toString()));
+            si.putExtra("b",Double.valueOf(teb.getText().toString()));
+            si.putExtra("c",Double.valueOf(tec.getText().toString()));
             startActivityForResult(si, 1);
         }
         else{
             Toast.makeText(this, "You forgot to input something", Toast.LENGTH_LONG).show();
         }
     }
-
+    @Override
+    protected void onActivityResult(int source, int good, @Nullable Intent data_back) {
+        if (data_back != null) {
+            if(data_back.getIntExtra("resam",0)==2) tv.setText("for a="+data_back.getDoubleExtra("a",0)+", b="+data_back.getDoubleExtra("b",0)+", c="+data_back.getDoubleExtra("c",0)+" results were:"+data_back.getDoubleExtra("x1",0)+", "+data_back.getDoubleExtra("x2",0));
+        }
+    }
     public void rand(View view) {
         a = ((double)rnd.nextInt(101)+(rnd.nextInt(101)/100));
         b = ((double)rnd.nextInt(101)+(rnd.nextInt(101)/100));
--- app/src/main/java/com/example/roots/rootsolve.java
+++ app/src/main/java/com/example/roots/rootsolve.java
@@ -4,13 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class rootsolve extends AppCompatActivity {
     Intent gi;
+    double a, b, c, x1, x2;
+    int resam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rootsolve);
+        gi=getIntent();
+        a = gi.getDoubleExtra("a", 1);
+        b = gi.getDoubleExtra("b", 0);
+        c = gi.getDoubleExtra("c", 1);
+        if(b*b>4*a*c){
+            resam = 2;
+        }
+        else if(b*b==4*a*c){
+            resam = 1;
+        }
+        else{
+            resam = 0;
+        }
+
+    }
+
+    public void ret(View view) {
+        gi.putExtra("a", a);
+        gi.putExtra("b", b);
+        gi.putExtra("c", c);
+        gi.putExtra("x1", x1);
+        gi.putExtra("x2", x2);
+        gi.putExtra("resam", resam);
+        setResult(RESULT_OK, gi);
+        finish();
     }
 }
 
